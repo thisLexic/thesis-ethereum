eb0869e68d30eb8c491804c814660c0122f600fb
Raphael Enriquez <raphael.enriquez@obf.ateneo.edu>
2021-10-05 15:50:03 +0800

    Added getCardInfo, getCardCount, and editCardInfo

--- truffle/contracts/Card.sol
+++ truffle/contracts/Card.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.7;
 import "./Branch.sol";
 
 contract Card is Branch {
+    
+    uint256 public cardCount; 
+
     struct PRCCard {
+        uint256 index;
         string name;
         uint256 idNumber;
         bool isValid;
@@ -21,27 +25,29 @@ contract Card is Branch {
 
     constructor() {
         owner = msg.sender;
+        cardCount = 0;
     }
 
     function createCard(
         string memory _name,
         uint256 _idNumber,
-        uint256 _idNumberBranch
-        string profession; 
-        uint256 dateOfBirth;
-        uint256 expirationDate;
-    ) public onlyOwner {
+        uint256 _idNumberBranch,
+        string memory _profession,
+        uint256 _dateOfBirth,
+        uint256 _expirationDate ) public onlyOwner {
         PrcBranch memory branch = branches[_idNumberBranch];
 
+        cards[_idNumber].index = cardCount+1;
         cards[_idNumber].name = _name;
         cards[_idNumber].idNumber = _idNumber;
+        cards[_idNumber].isValid = true;
         cards[_idNumber].profession = _profession;
         cards[_idNumber].dateOfBirth = _dateOfBirth;
         cards[_idNumber].expirationDate = _expirationDate;
-        cards[_idNumber].isValid = true;
         cards[_idNumber].issuedAt = branch;
         cards[_idNumber].issuedOnCount = 1;
         cards[_idNumber].issuedOn[1] = block.timestamp;
+        cardCount += 1;
     }
 
     function revalidateCard(uint256 _idNumber) public onlyOwner {
@@ -58,5 +64,23 @@ contract Card is Branch {
 
     function getCardIssuedOn(uint256 _idNumber) public view returns (uint256) {
         return cards[_idNumber].issuedOn[cards[_idNumber].issuedOnCount];
+    }
+
+    function getCardInfo(uint256 _idNumber) public view returns (uint256, string memory, bool, string memory, uint256, uint256){
+        PRCCard storage c = cards[_idNumber];
+        return (c.idNumber, c.name, c.isValid, c.profession, c.dateOfBirth, c.expirationDate); // Add branch
+    }
+
+    function getCardCount() public view returns (uint256) {
+        return cardCount;
+    }
+
+    function editCardInfo(uint256 _idNumber, string memory _name, bool _isValid, string memory _profession, uint256 _dateOfBirth, uint256 _expirationDate) public onlyOwner {
+        cards[_idNumber].idNumber = _idNumber;
+        cards[_idNumber].name = _name;
+        cards[_idNumber].isValid = _isValid;
+        cards[_idNumber].profession = _profession;
+        cards[_idNumber].dateOfBirth = _dateOfBirth;
+        cards[_idNumber].expirationDate = _expirationDate;
     }
 }
