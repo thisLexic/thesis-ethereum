7b41a5895b6adaf8a333458e401d03d1e85908e3
Raphael Enriquez <raphael.enriquez@obf.ateneo.edu>
2021-12-01 21:37:25 +0800

    Removed Branch files and deprecated variables

--- truffle/contracts/Card.sol
+++ truffle/contracts/Card.sol
@@ -3,24 +3,14 @@ pragma solidity ^0.8.7;
 import "./CardManager.sol";
 
 contract Card {
-    string public name;
     uint256 public idNumber;
-    bool public isValid;
-    string public branch;
-    uint256 public issuedOnCount;
-    mapping(uint256 => uint256) public issuedOn;
     string public ipfsHash;
 
     CardManager parentContract;
 
-    constructor(CardManager _parentContract, string memory _branch, uint256 _idNumber, string memory _name, string memory _ipfsHash) public {
+    constructor(CardManager _parentContract, uint256 _idNumber, string memory _ipfsHash) public {
         parentContract = _parentContract;
-        name = _name;
         idNumber = _idNumber;
-        isValid = true;
-        branch = _branch;
-        issuedOnCount = 1;
-        issuedOn[1] = block.timestamp;
         ipfsHash = _ipfsHash;
     }
 
--- truffle/contracts/CardManager.sol
+++ truffle/contracts/CardManager.sol
@@ -5,6 +5,7 @@ import "./Card.sol";
 contract CardManager {
     
     event CreateCardEvent(uint indexed _idNumber, string _ipfsHash);
+    event EditCardEvent(uint indexed _idNumber, string _ipfsHash);
     struct P_Card {
         Card card;
     }
@@ -12,6 +13,11 @@ contract CardManager {
     mapping(uint => P_Card) public cards;
     
     function createCard(string memory _branch, uint256 _idNumber,  string memory _name, string memory _ipfsHash) public {
+        cards[_idNumber].card = new Card(this, _branch, _idNumber, _name, _ipfsHash);
+        emit CreateCardEvent(_idNumber, _ipfsHash);
+    }
+
+    function editCard(string memory _branch, uint256 _idNumber,  string memory _name, string memory _ipfsHash) public {
         cards[_idNumber].card = new Card(this, _branch, _idNumber, _name, _ipfsHash);
         emit CreateCardEvent(_idNumber, _ipfsHash);
     }
